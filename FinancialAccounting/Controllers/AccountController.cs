0fb0992aed99b1727c8bbf078f333a051de4c4ca
Volkov-Serhii <serhei2016@gmail.com>
2023-07-04 22:24:22 +0300

    Add methodth of registration, Login and Logout

---
+++ FinancialAccounting/Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using FinancialAccounting.Models;
+using FinancialAccounting.Models.NoDBModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialAccounting.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async void Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email, FirstName = model.FirstName , LastName = model.LastName };
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    //return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            //return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async void Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    // проверяем, принадлежит ли URL приложению
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        //return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        //return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            //return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async void Logout()
+        {
+            // удаляем аутентификационные куки
+            await _signInManager.SignOutAsync();
+            //return RedirectToAction("Index", "Home");
+        }
+    }
+}
