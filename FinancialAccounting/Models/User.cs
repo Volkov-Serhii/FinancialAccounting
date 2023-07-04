32d5b2e3d1cbf10c23e63c72ed814ba98461b15d
Volkov-Serhii <serhei2016@gmail.com>
2023-07-04 17:19:28 +0300

    Add User

---
+++ FinancialAccounting/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FinancialAccounting.Models
+{
+    public class User : IdentityUser
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
