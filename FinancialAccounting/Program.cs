1c0292fab73f840835fd2106992987e1410b54c8
Volkov-Serhii <serhei2016@gmail.com>
2023-09-07 21:05:33 +0300

    Change BD to MySQL

--- FinancialAccounting/Program.cs
+++ FinancialAccounting/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +17,13 @@ builder.Services.AddCors(options =>
                .AllowAnyHeader();
     });
 });
-// Add services to the container.
+// Add services to the container(SQLServer).
+//builder.Services.AddDbContext<FinancialAccountingContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container(MySQL).
 builder.Services.AddDbContext<FinancialAccountingContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<FinancialAccountingContext>();
@@ -26,10 +31,10 @@ builder.Services.AddIdentity<User, IdentityRole>()
 builder.Services.AddControllersWithViews();
 
 //интеграция React приложения 
-builder.Services.AddSpaStaticFiles(configuration =>
-{
-    configuration.RootPath = "ClientApp/build"; // Путь к React приложению
-});
+//builder.Services.AddSpaStaticFiles(configuration =>
+//{
+//    configuration.RootPath = "ClientApp/build"; // Путь к React приложению
+//});
 
 var app = builder.Build();
 
@@ -47,19 +52,19 @@ app.UseRouting();
 app.UseCors();
 
 //интеграция React приложения
-app.UseSpaStaticFiles();
-app.Map("/app", builder =>
-{
-    builder.UseSpa(spa =>
-    {
-        spa.Options.SourcePath = "ClientApp"; // Путь к React приложению
+//app.UseSpaStaticFiles();
+//app.Map("/app", builder =>
+//{
+//    builder.UseSpa(spa =>
+//    {
+//        spa.Options.SourcePath = "ClientApp"; // Путь к React приложению
 
-        if (app.Environment.IsDevelopment())
-        {
-            spa.UseReactDevelopmentServer(npmScript: "start"); // Запустить React Development Server в режиме разработки
-        }
-    });
-});
+//        if (app.Environment.IsDevelopment())
+//        {
+//            spa.UseReactDevelopmentServer(npmScript: "start"); // Запустить React Development Server в режиме разработки
+//        }
+//    });
+//});
 //app.MapWhen(context => !context.Request.Path.StartsWithSegments("/api"), builder =>
 //{
 //    builder.UseSpa(spa =>
@@ -78,5 +83,12 @@ app.MapControllerRoute(
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html"); ;
+
+//Add migrations
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<FinancialAccountingContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
